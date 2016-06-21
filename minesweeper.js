c799cc872590ba992053773017682c06be18d227
siobhan-claire <sbhn.ocnnr@gmail>
2016-06-21 20:45:42 +1200

    removed hidden

--- minesweeper.js
+++ minesweeper.js
@@ -1,4 +1,23 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
-function startGame () {
+function startGame() {
+  var boardCells = document.getElementsByClassName('hidden');
+//console.log(boardCells.length)
+  for (i = 0; i < boardCells.length; i++){
+  addListeners(boardCells[i]);
+ }
 }
+
+function addListeners(element){
+  console.log(element)
+element.addEventListener('click', showCell);
+
+}
+
+function showCell(evt){
+    evt.target.classList.remove('hidden');
+}
+
+
+
+
