2139027dc2f004fdeef94bf648cfa8e46457e91f
siobhan-claire <sbhn.ocnnr@gmail>
2016-06-21 21:26:42 +1200

    applied ! to cells

--- minesweeper.js
+++ minesweeper.js
@@ -2,21 +2,28 @@ document.addEventListener('DOMContentLoaded', startGame)
 
 function startGame() {
   var boardCells = document.getElementsByClassName('hidden');
-//console.log(boardCells.length)
+
   for (i = 0; i < boardCells.length; i++){
   addListeners(boardCells[i]);
  }
 }
 
 function addListeners(element){
-  console.log(element)
+  
 element.addEventListener('click', showCell);
+element.addEventListener('contextmenu', markCell);
 
 }
 
 function showCell(evt){
     evt.target.classList.remove('hidden');
 }
+
+function markCell(evt){
+    evt.preventDefault();
+    evt.target.classList.toggle('marked');
+}
+
 
 
 
