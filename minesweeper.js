93f04acbd6928e028cefff902a401129c3388565
siobhan-claire <sbhn.ocnnr@gmail>
2016-06-24 16:38:20 +1200

    looped through row and col

--- minesweeper.js
+++ minesweeper.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
 function startGame() {
-  var boardCells = document.getElementsByClassName('hidden');
-
-  for (i = 0; i < boardCells.length; i++){
-  addListeners(boardCells[i]);
- }
+  var boardCells = document.getElementsByClassName('hidden')
+  for (var i = 0; i < boardCells.length; i++) {
+    addListeners(boardCells[i])
+    console.log(getRow(boardCells[i]))
+   }
 }
 
 function addListeners(element){
@@ -15,16 +15,49 @@ element.addEventListener('contextmenu', markCell);
 
 }
 
+//create function showCell
 function showCell(evt){
     evt.target.classList.remove('hidden');
 }
 
+//create function markCell
 function markCell(evt){
     evt.preventDefault();
     evt.target.classList.toggle('marked');
 }
 
+var board = {cell: []};
 
+function getRow(element){
+    var classes = element.classList
+    for (var i = 0; i < classes.length; i++) {
+      if (classes[i].substring(0,3) === "row") {
+        var c = classes[i] 
+        var arraySplit = c.split("-")
+        var rowString = arraySplit[1]
+        var row = parseInt(rowString)
+        return row 
+      }
+  }
+}
 
+function getCol (element) {
+  var classes = element.classList
+  for (var i = 0; i < classes.length; i++) {
+    if (classes[i].substring(0,3) === "col") {
+      var c = classes[i] 
+      var arraySplit = c.split("-")
+      var colString = arraySplit[1]
+      var col = parseInt(colString)
+      return col 
+    }
+  }
+}
 
+  
+ 
 
+function addCellToBoard(element){
+  var newCell = {};
+
+}
